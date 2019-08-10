db2d7418cbcaf1aae0646eb55594192771bb10ab
Alex Trzeciak <trebek08@gmail.com>
2019-08-10 15:59:55 -0700

    implement the chacaterCollection sorting method

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+const numberSorter = new Sorter(numbersCollection);
+numberSorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("Xaayayabbaccdrobotron");
+const characterSort = new Sorter(charactersCollection);
+characterSort.sort();
+console.log(charactersCollection.data);
