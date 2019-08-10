8593b28868211a2bfacc0a8a85318c06b1866f68
Alex Trzeciak <trebek08@gmail.com>
2019-08-10 16:22:04 -0700

    linkedList sort

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const numberSorter = new Sorter(numbersCollection);
@@ -11,3 +12,12 @@ const charactersCollection = new CharactersCollection("Xaayayabbaccdrobotron");
 const characterSort = new Sorter(charactersCollection);
 characterSort.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+const linkedSorter = new Sorter(linkedList);
+linkedSorter.sort();
+linkedList.print();
