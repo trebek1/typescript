ba04e14dd039a2d69154ce44bf8baeba003d78ce
Alex Trzeciak <trebek08@gmail.com>
2019-08-11 14:21:33 -0700

    Break the logic for printing into its own summary clasas

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,19 +1,14 @@
 import { MatchReader } from "./MatchReader";
+import { CsvFileReader } from "./CsvFileReader";
 import { MatchResult } from "./MatchResult";
 
-const reader = new MatchReader("football.csv");
-reader.read();
+// const reader = new MatchReader("football.csv");
+// reader.read();
+
+const csvFileReader = new CsvFileReader("football.csv");
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 // enum - enumeration
 
-let manUnitedWins = 0;
-
-for (let match of reader.data) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(manUnitedWins);
+// console.log(manUnitedWins);
