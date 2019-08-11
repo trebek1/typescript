4e27a1e1b5dd21289186aa630fe37c94063d41ac
Alex Trzeciak <trebek08@gmail.com>
2019-08-10 19:56:56 -0700

    setup stats project and parse data

---
+++ stats/src/index.ts
@@ -0,0 +1,28 @@
+import fs from "fs";
+
+const matches = fs
+  .readFileSync("football.csv", {
+    encoding: "utf-8"
+  })
+  .split("\n")
+  .map((row: string): string[] => row.split(","));
+
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D"
+}
+
+// enum - enumeration
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(manUnitedWins);
