fc11aa804caf136edefd195ab742e8492fc9f965
Alex Trzeciak <trebek08@gmail.com>
2019-08-13 19:37:26 -0700

    refactor impl of on trigger get and set

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,10 +2,11 @@ import { User } from "./models/User";
 
 const user = new User({ name: "new rec", age: 0 });
 
-// user.set({ name: "NEW NAME", age: 999 });
-user.save();
+console.log(user.get("name"));
 
-user.events.on("change", () => {
-  console.log("hihihi");
-});
-user.events.trigger("change");
+// reminder on how this works in js
+
+user.on("change", () => console.log("abc"));
+
+// user.trigger("change");
+user.set({ name: "new name" });
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
+import { Attributes } from "./Attributes";
 
 export interface UserProps {
   name?: string;
@@ -12,4 +13,26 @@ const rootUrl = "http://localhost:3000/users";
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>("rootUrl");
+  public attributes: Attributes<UserProps>;
+
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.events.trigger("change");
+  }
 }
