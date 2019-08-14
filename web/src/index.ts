54455b35d34795492e52a8a3f67e287c104d2eb2
Alex Trzeciak <trebek08@gmail.com>
2019-08-13 20:51:58 -0700

    finish user class using composition

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,7 +1,7 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1 });
+const user = new User({ id: 1, name: "newer name", age: 0 });
 
-user.on("change", () => console.log(user));
+user.on("save", () => console.log(user));
 
-user.fetch();
+user.save();
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -46,4 +46,15 @@ export class User {
       this.set(resp.data);
     });
   }
+
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then(
+        (resp: AxiosResponse): void => {
+          this.trigger("save");
+        }
+      )
+      .catch(() => this.trigger("error"));
+  }
 }
